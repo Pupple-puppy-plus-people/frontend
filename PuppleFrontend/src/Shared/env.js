148700381773b71837b7e8479eeb2517454194d3
Kchanghun <abcd877287@gmail.com>
2022-05-13 14:24:51 +0900

    show detail about wishlist

--- PuppleFrontend/src/Shared/env.js
+++ PuppleFrontend/src/Shared/env.js
@@ -1,4 +1,5 @@
-export const HS_API_END_POINT = 'http://52.79.229.200:8000';
+// export const HS_API_END_POINT = 'http://52.79.229.200:8000';
+export const HS_API_END_POINT = 'http://127.0.0.1:8000';
 import {useState} from 'react'
 export const USER_INFO=()=>{
     // const [USER_EMAIL,setUSER_EMAIL] = useState("");
