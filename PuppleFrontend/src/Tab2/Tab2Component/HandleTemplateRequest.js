5f336e7b9a1330bbb9b3a697c279ccadf8808a4f
Kchanghun <abcd877287@gmail.com>
2022-05-22 20:52:05 +0900

    show room image in flatlist

--- PuppleFrontend/src/Tab2/Tab2Component/HandleTemplateRequest.js
+++ PuppleFrontend/src/Tab2/Tab2Component/HandleTemplateRequest.js
@@ -1,13 +1,24 @@
-import React, {useState} from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import {
     StyleSheet,
     View,
     Text,
     Image,
-    Pressable
+    Pressable,
+    FlatList,
+    Modal,
 } from 'react-native';
-
+import { responsiveScreenFontSize, responsiveScreenHeight, responsiveScreenWidth, responsiveWidth } from 'react-native-responsive-dimensions';
 import { HS_API_END_POINT, USER_INFO } from '../../Shared/env';
+
+// temp data
+let temp_image = [
+    'iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg==',
+    'iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='
+]
+
+
 
 const HandleTemplateReqeust = ({navigation, aboutDog}) => {
     const [parentWidth, setParentWidth] = useState({width:0});
@@ -17,11 +28,41 @@ const HandleTemplateReqeust = ({navigation, aboutDog}) => {
         setParentHeight({height:height});
         setParentWidth({width:width});
     }
+    useEffect(()=>{
+        // axios.post(`${HS_API_END_POINT}/api/housephoto/`)
+    })
     return (
         <View style={{}} onLayout={onLayout}>
-            <Text>{aboutDog.name}</Text>
+            {/* <Modal
+            animationType='slide'
+            transparent={false}
+            visible={false}
+            onRequestClose={()=>{
+
+            }}>
+
+            </Modal> */}
+            <FlatList
+            data={temp_image}
+            renderItem={renderImage}
+            listKey={new Date().getTime().toString()}
+            />
         </View>
     )
 }
 
+const renderImage = ({item}) =>{
+    return(
+        <View>
+            <Text>a</Text>
+            <Image style={{width:responsiveScreenWidth(24),height:responsiveScreenWidth(24)}} 
+                    source={{uri:`data:image/jpeg;base64,${item}`}}/>
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+
+})
 export default HandleTemplateReqeust;
